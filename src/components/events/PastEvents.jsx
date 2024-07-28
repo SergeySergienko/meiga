c1738abefe4bd67d088cb31b95ff345f74af2d23
Sergey <serg.v.sergienko@gmail.com>
2024-07-28 21:54:15 +0200

    Move events images to public follder

--- src/components/events/PastEvents.jsx
+++ src/components/events/PastEvents.jsx
@@ -1,14 +1,10 @@
 import { EventCardCover } from './EventCardCover';
 
-const photos_08_26_2023 = import.meta.glob(
-  '/src/assets/images/events/08_26_2023/*'
-);
-const photos_07_03_2024 = import.meta.glob(
-  '/src/assets/images/events/07_03_2024/*'
-);
-const photos_07_17_2024 = import.meta.glob(
-  '/src/assets/images/events/07_17_2024/*'
-);
+const getPublicUrl = (url) => url.replace('/public', '');
+
+const photos_08_26_2023 = import.meta.glob('/public/events/08_26_2023/*');
+const photos_07_03_2024 = import.meta.glob('/public/events/07_03_2024/*');
+const photos_07_17_2024 = import.meta.glob('/public/events/07_17_2024/*');
 
 const events = [
   {
@@ -16,14 +12,14 @@ const events = [
     date: '07/17/2024',
     location: 'Irgendwo im Raum Freiberg',
     description: 'Typisches Samstagstraining im Sand.',
-    photos: Object.keys(photos_07_17_2024),
+    photos: Object.keys(photos_07_17_2024).map(getPublicUrl),
   },
   {
     title: 'Training',
     date: '07/03/2024',
     location: 'Raum Freiberg',
     description: 'Gewöhnliches Samstagstraining im Freien.',
-    photos: Object.keys(photos_07_03_2024),
+    photos: Object.keys(photos_07_03_2024).map(getPublicUrl),
   },
   {
     title: 'Freundschaft turnier',
@@ -31,7 +27,7 @@ const events = [
     location: 'Raum Freiberg',
     description:
       'Abschluss der Freiluftsaison mit einem Turnier mit Teilnahme unserer Volleyball - Freunde aus Dresden.',
-    photos: Object.keys(photos_08_26_2023),
+    photos: Object.keys(photos_08_26_2023).map(getPublicUrl),
   },
 ];
 
